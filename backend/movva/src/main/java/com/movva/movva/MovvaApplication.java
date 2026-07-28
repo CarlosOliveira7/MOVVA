e8bdf415afb150874df0261e0ad11d4f739f8cca
CarlosOliveira7 <devcarlos200@gmail.com>
2026-07-28 11:26:31 -0300

    chore: added folder structure for monorepo and added spring backend

---
+++ backend/movva/src/main/java/com/movva/movva/MovvaApplication.java
@@ -0,0 +1,13 @@
+package com.movva.movva;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovvaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovvaApplication.class, args);
+	}
+
+}
---
+++ backend/movva/src/test/java/com/movva/movva/MovvaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.movva.movva;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovvaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
